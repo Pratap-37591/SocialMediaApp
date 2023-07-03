45dc0d192b6b3c69cab379fcd6e59377f39bccbe
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-03 11:03:56 +0530

    protected  route and react router

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,58 @@ import NavBar from "./components/navbar/NavBar";
 import RightBar from "./components/rightBar/RightBar";
 import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Profile from "./pages/profile/Profile";
 import {
   createBrowserRouter,
   RouterProvider,
   Route,
   Outlet,
+  Navigate,
 } from "react-router-dom";
 
+const currentUser = true;
+
+const Layout = () => {
+  return (
+    <div>
+      <NavBar />
+      <div style={{ display: "flex" }}>
+        <LeftBar />
+        <Outlet />
+        <RightBar />
+      </div>
+    </div>
+  );
+};
+
+const ProtectedRoute = ({ children }) => {
+  if (!currentUser) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <ProtectedRoute>
+        <Layout />
+      </ProtectedRoute>
+    ),
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/profile/:id",
+        element: <Profile />,
+      },
+    ],
+  },
   {
     path: "/login",
     element: <Login />,
@@ -21,18 +65,6 @@ const router = createBrowserRouter([
   },
 ]);
 
-const Layout = () => {
-  return (
-    <div>
-      <NavBar />
-      <div style={{display: 'flex'}}>
-        <LeftBar />
-        <Outlet />
-        <RightBar />
-      </div>
-    </div>
-  );
-};
 function App() {
   return (
     <div>
