2de410e9320f9c496a9e811784202530c08f8bf5
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-03 11:04:18 +0530

    navbar created

--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import './navbar.scss';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
+import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
+import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
+import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
+import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='navbar'>
+      <div className="left">
+        <Link to="/" style={{textDecoration: 'none'}}>
+          <span>PostBook</span>
+        </Link>
+        <HomeOutlinedIcon/>
+        <DarkModeOutlinedIcon/>
+        <GridViewOutlinedIcon/>
+        <div className="search">
+          <SearchOutlinedIcon/>
+          <input type="text" placeholder='Search...' />
+        </div>
+      </div>
+      <div className="right">
+        <PersonOutlineOutlinedIcon/>
+        <EmailOutlinedIcon/>
+        <NotificationsOutlinedIcon/>
+        <div className="user">
+          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="profile" />
+        <span>Pratap Hangodi</span>
+        </div>
+      </div>
+    </div>
   )
 }
 
