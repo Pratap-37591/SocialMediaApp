4fd5b824ca8dd22e53dfc7f947d429d9dda9fe5b
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-15 22:06:42 +0530

    rightbar styles added

--- src/components/rightBar/RightBar.jsx
+++ src/components/rightBar/RightBar.jsx
@@ -42,11 +42,120 @@ const RightBar = () => {
                 alt="mensprofile"
               />
               <p>
-                <span>John sena </span> Changed Profile picture
-                </p>
+                <span>John sena</span> liked your post
+              </p>
             </div>
-          <span>1 min ago</span>   
- </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <p>
+                <span>John sena</span> updated is profile
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <p>
+                <span>John sena</span> shared a post
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <p>
+                <span>John sena</span> changed profile picture
+              </p>
+            </div>
+            <span>1 min ago</span>
+          </div>
+        </div>
+        <div className="item">
+          <span>Online Friends</span>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
+          <div className="user">
+            <div className="user-info">
+              <img
+                src="https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=600"
+                alt="mensprofile"
+              />
+              <div className="online" />
+              <span>John sena</span>
+            </div>
+          </div>
         </div>
       </div>
     </div>
