072c0777520d03d9dabc4614c4abc7ee1af6f122
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-17 22:56:26 +0530

    darkMode features created and  props passing through context api and user login info context created

--- src/App.jsx
+++ src/App.jsx
@@ -12,63 +12,68 @@ import {
   Outlet,
   Navigate,
 } from "react-router-dom";
-import './style.scss';
-
-const currentUser = true;
-
-const Layout = () => {
-  return (
-    <div className="theme-dark">
-      <NavBar />
-      <div style={{ display: "flex" }}>
-        <LeftBar />
-        <div style={{flex: 6}}>
-          <Outlet />
-        </div>
-        <RightBar />
-      </div>
-    </div>
-  );
-};
-
-const ProtectedRoute = ({ children }) => {
-  if (!currentUser) {
-    return <Navigate to="/login" />;
-  }
-
-  return children;
-};
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: (
-      <ProtectedRoute>
-        <Layout />
-      </ProtectedRoute>
-    ),
-    children: [
-      {
-        path: "/",
-        element: <Home />,
-      },
-      {
-        path: "/profile/:id",
-        element: <Profile />,
-      },
-    ],
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-]);
+import "./style.scss";
+import { useContext } from "react";
+import { DarkModeContext } from "./components/context/darkModeContext";
+import { AuthContext } from "./components/context/authContext";
 
 function App() {
+  const {currentUser} = useContext(AuthContext);
+
+  const { darkMode } = useContext(DarkModeContext);
+
+  const Layout = () => {
+    return (
+      <div className={`theme-${darkMode ? "dark" : "light"}`}>
+        <NavBar />
+        <div style={{ display: "flex" }}>
+          <LeftBar />
+          <div style={{ flex: 6 }}>
+            <Outlet />
+          </div>
+          <RightBar />
+        </div>
+      </div>
+    );
+  };
+
+  const ProtectedRoute = ({ children }) => {
+    if (!currentUser) {
+      return <Navigate to="/login" />;
+    }
+
+    return children;
+  };
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
+      children: [
+        {
+          path: "/",
+          element: <Home />,
+        },
+        {
+          path: "/profile/:id",
+          element: <Profile />,
+        },
+      ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/register",
+      element: <Register />,
+    },
+  ]);
+
   return (
     <div>
       <RouterProvider router={router} />
--- src/components/navbar/NavBar.jsx
+++ src/components/navbar/NavBar.jsx
@@ -1,40 +1,53 @@
-import './navbar.scss';
-import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
-import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
-import WbSunnyOutlinedIcon from '@mui/icons-material/WbSunnyOutlined';
-import GridViewOutlinedIcon from '@mui/icons-material/GridViewOutlined';
-import SearchOutlinedIcon from '@mui/icons-material/SearchOutlined';
-import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
-import PersonOutlineOutlinedIcon from '@mui/icons-material/PersonOutlineOutlined';
-import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
-import { Link } from 'react-router-dom';
+import "./navbar.scss";
+import HomeOutlinedIcon from "@mui/icons-material/HomeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import WbSunnyOutlinedIcon from "@mui/icons-material/WbSunnyOutlined";
+import GridViewOutlinedIcon from "@mui/icons-material/GridViewOutlined";
+import SearchOutlinedIcon from "@mui/icons-material/SearchOutlined";
+import EmailOutlinedIcon from "@mui/icons-material/EmailOutlined";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import { Link } from "react-router-dom";
+import { useContext } from "react";
+import { DarkModeContext } from "../context/darkModeContext";
+import { AuthContext } from "../context/authContext";
 
 const NavBar = () => {
+  const { toggle, darkMode } = useContext(DarkModeContext);
+  const { currentUser } = useContext(AuthContext);
+
   return (
-    <div className='navbar'>
+    <div className="navbar">
       <div className="left">
-        <Link to="/" style={{textDecoration: 'none'}}>
+        <Link to="/" style={{ textDecoration: "none" }}>
           <span>PostBook</span>
         </Link>
-        <HomeOutlinedIcon/>
-        <DarkModeOutlinedIcon/>
-        <GridViewOutlinedIcon/>
+        <HomeOutlinedIcon />
+        {darkMode ? (
+          <WbSunnyOutlinedIcon onClick={toggle} />
+        ) : (
+          <DarkModeOutlinedIcon onClick={toggle} />
+        )}
+        <GridViewOutlinedIcon />
         <div className="search">
-          <SearchOutlinedIcon/>
-          <input type="text" placeholder='Search...' />
+          <SearchOutlinedIcon />
+          <input type="text" placeholder="Search..." />
         </div>
       </div>
       <div className="right">
-        <PersonOutlineOutlinedIcon/>
-        <EmailOutlinedIcon/>
-        <NotificationsOutlinedIcon/>
+        <PersonOutlineOutlinedIcon />
+        <EmailOutlinedIcon />
+        <NotificationsOutlinedIcon />
         <div className="user">
-          <img src="https://images.pexels.com/photos/3228727/pexels-photo-3228727.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="profile" />
-        <span>Pratap Hangodi</span>
+          <img
+            src={currentUser.profilePicture}
+            alt="profile"
+          />
+          <span>{currentUser.name}</span>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default NavBar;
