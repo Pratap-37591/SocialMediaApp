f518267b1243d2e99c56da5180b479f76ebc838f
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-17 22:58:40 +0530

    posts section created

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,61 @@
+import React, { useState } from "react";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import FavoriteOutlinedIcon from "@mui/icons-material/FavoriteOutlined";
+import TextsmsOutlinedIcon from "@mui/icons-material/TextsmsOutlined";
+import ShareOutlinedIcon from "@mui/icons-material/ShareOutlined";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import { Link } from "react-router-dom";
+import "./post.scss";
+import Comments from "../comments/Comments.jsx";
+
+
+const Post = ({ post }) => {
+  
+  const [commentOpen, setCommentOpen] = useState(false);
+
+  //Tempo
+  const like = false;
+
+  return (
+    <div className="post">
+      <div className="container">
+        <div className="user">
+          <div className="userInfo">
+            <img src={post.profilePic} alt="post" />
+            <div className="details">
+              <Link
+                to={`/profile/${post.userId}`}
+                style={{ textDecoration: "none", color: "inherit" }}
+              >
+                <span className="name">{post.name}</span>
+              </Link>
+              <span className="date">a few seconds ago</span>
+            </div>
+          </div>
+          <MoreHorizIcon />
+        </div>
+        <div className="content">
+          <p>{post.desc}</p>
+          <img src={post.img} alt="imagepost" />
+        </div>
+        <div className="info">
+          <div className="item">
+            {like ? <FavoriteOutlinedIcon /> : <FavoriteBorderOutlinedIcon />}
+            1k Likes
+          </div>
+          <div className="item" onClick={() => setCommentOpen(!commentOpen)}>
+            <TextsmsOutlinedIcon  />
+            1200 comments
+          </div>
+          <div className="item">
+            <ShareOutlinedIcon />
+            Share
+          </div>
+        </div>
+        {commentOpen && <Comments />}
+      </div>
+    </div>
+  );
+};
+
+export default Post;
