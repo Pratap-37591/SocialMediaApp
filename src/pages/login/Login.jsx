030d0be5141760c17c44fc3a572a156bedb562b4
Pratap-37591 <prathaphb1310@gmail.com>
2023-07-25 00:02:04 +0530

    login updated

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import "./login.scss";
 import { useContext, useState } from "react";
 import { AuthContext } from "../../components/context/authContext";
@@ -11,6 +11,8 @@ const Login = () => {
 
   const [err, setErr] = useState(null);
 
+  const navigate = useNavigate();
+
   const handleChange = (e) => {
     setInputs((prev) => ({ ...prev, [e.target.name]: e.target.value }));
   };
@@ -21,8 +23,13 @@ const Login = () => {
     e.preventDefault();
     try {
       await login(inputs);
+      navigate("/")
     } catch (err) {
-      setErr(err.response.data);
+      if (err.response && err.response.data) {
+        setErr(err.response.data);
+      } else {
+        console.error("An error occurred during login:", err);
+      }
     }
   };
 
